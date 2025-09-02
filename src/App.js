d194ffe75ab17618b4256ce8b47ee01bc3bc90f2
clemencerds <clemence.ray@hotmail.com>
2025-09-02 16:22:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import About from './components/About/About.js';
+import Portfolio from './components/Portfolio/Portfolio.js'
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<App/>}>
+    <Route path='/about' element={<About/>} />
+    <Route path='portfolio' element={<Portfolio/>} />
+  </Route>
+));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router ={ router } />
   );
-}
+};
 
 export default App;
